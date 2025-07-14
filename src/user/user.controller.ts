a1a89902eee26923135245e5b62e862426e3155a
Kitrishvili <kitrikitri69@gmail.com>
2025-07-14 18:55:57 +0400

    1.19

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth-decorator';
+import { currentUser } from './decorators/user.decorator';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Auth()
+  @Get('profile')
+  async getProfile(@currentUser('id') id:string){
+    return this.userService.getById(id)
+  }
+
+  @Auth()
+  @Get('profile/favorites/:productId')
+  async toggleFavorite(
+    @currentUser('id') userId:string,
+    @Param('productId') productId:string
+  ){
+    return this.userService.toggleFavorite(productId, userId)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/app.service';
 import { hash } from 'argon2'
 import { AuthDto } from 'src/auth/dto/auth.dto';
+import { truncate } from 'fs';
 
 @Injectable()
 export class UserService {
@@ -39,6 +40,29 @@ export class UserService {
             }
         })
         return user
+    }
+
+    async toggleFavorite(productId: string, userId:string){
+        const user = await this.getById(userId)
+
+        const isExists = user.favorites.some(
+            product => product.id === productId
+        )
+
+        await this.prisma.user.update({
+            where:{
+                id:user.id
+            },
+            data:{
+                favorites:{
+                    [isExists ? 'disconnect' : 'connect']:{
+                        id:productId
+                    }
+                }
+            }
+            
+        })
+        return true
     }
 
     async create (dto:AuthDto){
